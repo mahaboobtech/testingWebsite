9731ab15233ef171daec3c8b70cfa1498a0a6a6a
mahaboobtech <mickelbasha1234@gmail.com>
2023-04-10 23:14:46 +0530

    justTesting

--- Responsive-Website-Design/README.md
+++
@@ -1,4 +0,0 @@
-# Responsive-Website-Design
-This is the source code for the design of the complete website series on GTCoding YouTube channel.
-
-Here is the demo: https://mahaboobtech.github.io/testingWebsite/
--- main.js
+++ main.js
@@ -22,3 +22,12 @@ menuIcon.addEventListener('click', function () {
     slideoutMenu.style.pointerEvents = 'auto';
   }
 })
+
+function showcase() {
+  var content = document.getElementById("content");
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
