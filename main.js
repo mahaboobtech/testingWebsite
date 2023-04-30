335e501f412e5c44834de6c15c6da239e2191326
mahaboobtech <mickelbasha1234@gmail.com>
2023-04-30 17:48:51 +0530

    allbugsFixed

--- main.js
+++ main.js
@@ -31,3 +31,39 @@ function showcase() {
     content.style.display = "none";
   }
 }
+
+function scrollToBottom() {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth'
+  });
+}
+
+// Select all the links with a class of "scroll-link"
+const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+// Loop through the links and add a click event listener to each one
+scrollLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Get the target section ID from the href attribute
+    const targetId = link.getAttribute('href').slice(1);
+
+    // Construct the URL for the target page
+    const targetUrl = `${window.location.origin}/testingWebsite#${targetId}`;
+
+    // Navigate to the target page
+    window.location.href = targetUrl;
+
+    // Wait for the page to load, then scroll to the target section
+    window.addEventListener('load', () => {
+      const targetSection = document.getElementById(targetId);
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+});
+
+
+
